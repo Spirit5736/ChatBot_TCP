b4cdadca4c299c61b885ba0bbc02c94de32bf492
Spirit5736 <kozhevnikovi@mail.ru>
2020-12-12 17:12:26 +0700

    ChatBot

---
+++ ChatBot_TCP/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace ChatServer
+{
+    class Program
+    {
+        // сервер
+        static ServerObject server;
+        // потока для прослушивания
+        static Thread listenThread; 
+        static void Main(string[] args)
+        {
+            try
+            {
+                server = new ServerObject();
+                listenThread = new Thread(new ThreadStart(server.Listen));
+                //старт потока
+                listenThread.Start(); 
+            }
+            catch (Exception ex)
+            {
+                server.Disconnect();
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
